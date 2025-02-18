7cb753a2b2b15b02f1eebf3613650e59202ca726
Sneha <sriwastavasneha@gmail.com>
2025-02-18 16:59:42 +0530

    register page

---
+++ middleware/query_and_params/lec_18.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+
+app.use(express.urlencoded({extended:true}))
+//converts urlencoded data into objects
+// it is a middleware thant needs to be run on every request
+let userData=[]
+app.get("/adduser",(req,res)=>{
+    res.sendFile(__dirname+"/register.html")
+})
+app.post("/adduser",(req,res)=>{
+    let{name,email,password}=req.body;
+    userData.push({name:name,email:email,password:password})
+    console.log(name,email,password);
+    res.send(userData)
+})
+
+
+app.listen(9876,()=>{
+    console.log("server started")
+})
